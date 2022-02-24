87b0e49482e1bd270de4fa99b0e058d6b4e2c54b
Laboratório do IFRN <ifrnlab@gmail.com>
2022-02-24 09:15:38 -0300

    Remoção da parte de envio para Oulu

--- scripts/coleta-ifrn-redes-2021.sh
+++ scripts/coleta-ifrn-redes-2021.sh
@@ -62,60 +62,10 @@ netstat -an -p udp > conexoes-udp.txt
 echo "Vamos entrar no VS Code para revisar os dados coletados?"
 code .
 
-# https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
-# Confirmação de envio para Oulu
-read -p "Deseja enviar os dados para Oulu (S/N)?" -n 1 -r
-echo
-if [[ ${REPLY} =~ ^[Ss]$ ]]
-then
-
-cat << FIM
-CRIAÇÃO DE USUÁRIO NA OULU
-==========================
-
-Quando solicitado, entre com os seguintes dados:
-- Senha de estudante: estudante (Será solicitada 2x)
-- Matrícula: ${matricula}
-- Senha: <Sua senha do SUAP>
-
-Observe que não aparecerá nenhum caracter enquanto você digitar as senhas.
-
-Importante! Copie a senha nova que foi gerada para seu usuário. Você precisará 
-entrar com ela duas vezes (2x) antes de escolher sua senha na Oulu. 
-FIM
-
-ssh -t estudante@oulu.ifrn.edu.br sudo cria-meu-usuario
-
-cat << FIM
-TROCA DE SENHA DE SEU USUÁRIO NA OULU
-=====================================
-
-1. Copie a senha nova que voi criada para seu usuário na Oulu
-2. Cole a senha e pressione a tecla ENTER posteriormente
-
-   Para copiar ou colar a senha, use o botão direito do mouse.
-   
-   Lembre-se:
-   a) Copiar em inglês: "Copy"
-   b) Colar em inglês: "Paste"
-
-3. Escolha uma nova senha e entre com ela duas vezes (2x).
-
-OBS: Se der erro nesse passo, execute novamente o script.
-FIM
-
-ssh ${CONTA_OULU}@${FQDN_OULU} byobu-enable
-
-echo "Copactando os dados coletados..."
-cd ..
-tar zcvf ${NOME_DIR_COLETAS}.tar.gz ${NOME_DIR_COLETAS}
-
-echo "Enviando dados para Oulu..."
-scp ${NOME_DIR_COLETAS}.tar.gz ${CONTA_OULU}@${FQDN_OULU}:/repo/info3v2021/nossa-rede
-
-# Final do bloco IF
-fi
-
+echo "Por gentileza, abra, no Windows Explorer a pasta:"
+echo "    Documentos\Ifrn\Redes"
+echo 
+echo "E veja envie o arquivo de coleta pelo Google Sala de Aula."
 
 echo "Pressione a tecla <ENTER> para sair."
 
