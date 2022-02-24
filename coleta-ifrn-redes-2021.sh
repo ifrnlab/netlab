8ae5e7824ce3ac3ccc179a5d79c9358bbb9850c9
Laboratório do IFRN <ifrnlab@users.noreply.github.com>
2022-02-24 00:35:05 -0300

    Update coleta-ifrn-redes-2021.sh

--- coleta-ifrn-redes-2021.sh
+++ coleta-ifrn-redes-2021.sh
@@ -43,7 +43,7 @@ cd ..
 tar zcvf coleta-${matricula}.tar.gz Coleta-${matricula}
 
 echo "Por gentileza, abra, em seu Explorador do Windows (Windows Explorer), a pasta:"
-echo ${PWD}
+echo ${CWD_BI:1:1}:${CWD_BI:2:60}
 
 echo "E envie o arquivo coleta-${matricula}.tar.gz"
 read
