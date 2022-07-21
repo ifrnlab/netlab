3a1246a0833cbee77f79bbac858bf8b19d7b6241
Laboratório do IFRN <ifrnlab@users.noreply.github.com>
2022-07-21 12:50:09 -0300

    Update hosts-windows.ps1

--- scripts/hosts-windows.ps1
+++ scripts/hosts-windows.ps1
@@ -1,1 +1,4 @@
-Start-Process notepad -Verb runAs -ArgumentList $Env:windir\System32\drivers\etc\hosts
+Start-Process notepad -Verb runAs -ArgumentList "$Env:windir\System32\drivers\etc\hosts"
+
+# Referência:
+# - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.2
